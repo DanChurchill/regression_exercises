dffd89bfd5d3e058ca5f37b344a4e077536291e8
Dan Churchill <dan.churchill.satx@gmail.com>
2022-08-30 16:31:26 -0500

    completed scaling lesson and added functions to wrangle.py

--- wrangle.py
+++ wrangle.py
@@ -1,7 +1,12 @@
+from re import M
 import env
 import os
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, QuantileTransformer
+
 
 def get_zillow():
     '''
@@ -38,3 +43,66 @@ def prep_zillow(df):
        '''
        df = df.dropna()
        return df
+
+def my_split(df):
+       '''
+       Separates a dataframe into train, validate, and test datasets
+
+       Keyword arguments:
+       df: a dataframe containing multiple rows
+       
+
+       Returns:
+       three dataframes who's length is 60%, 20%, and 20% of the length of the original dataframe       
+       '''
+
+       # separate into 80% train/validate and test data
+       train_validate, test = train_test_split(df, test_size=.2, random_state=333)
+
+       # further separate the train/validate data into train and validate
+       train, validate = train_test_split(train_validate, 
+                                          test_size=.25, 
+                                          random_state=333)
+
+       return train, validate, test
+
+def remove_outliers(df, k, col_list):
+    ''' remove outliers from a list of columns in a dataframe 
+        and return that dataframe
+    '''
+    
+    for col in col_list:
+
+        q1, q3 = df[col].quantile([.25, .75])  # get quartiles
+        
+        iqr = q3 - q1   # calculate interquartile range
+        
+        upper_bound = q3 + k * iqr   # get upper bound
+        lower_bound = q1 - k * iqr   # get lower bound
+
+        # return dataframe without outliers
+        
+        df = df[(df[col] > lower_bound) & (df[col] < upper_bound)]
+        
+    return df
+
+def min_max_scaler(train, validate, test):
+       '''
+       function accepts train, validate, and test dataframes and 
+       performs min_max scaling on the sq_ft and tax_amount columns
+       returns the train, validate, and test dataframes with the additional 
+       scaled columns
+       '''
+       scaler = MinMaxScaler()
+       
+
+       train[['scaled_sqft', 'scaled_taxamount']] = scaler.fit_transform(train[['calculatedfinishedsquarefeet',
+                                                                         'taxamount']])
+
+       validate[['scaled_sqft', 'scaled_taxamount']] = scaler.transform(validate[['calculatedfinishedsquarefeet',
+                                                                         'taxamount']])
+
+       test[['scaled_sqft', 'scaled_taxamount']] = scaler.transform(test[['calculatedfinishedsquarefeet',
+                                                                         'taxamount']])
+
+       return train, validate, test
